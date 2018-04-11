ad0a4d673f2e25dbcf0076bfcae9dbb5b5266711
nellybaz <35724948+nellybaz@users.noreply.github.com>
2018-04-11 11:57:27 +0200

    Hng internship code, stage one: making a php simple page showing current time

---
+++ hng-stg-1.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>sql hacking</title>
+
+  <style type="text/css">
+  body{
+  	text-align: center;
+  }
+  *{
+    margin: 0 auto;
+  }
+  	#date{
+  		margin-left: 30%;
+  		margin-top: 15%;
+  		text-align: center;
+  		
+  		width:40%;
+  		height:300px;
+  		font-size: 4rem;
+  		padding: 10px;
+      color: #2F80ED;
+  	}
+     .h1{
+
+      width: 100%;
+      background-color: #A36464;
+      color: white;
+      padding: 40px;
+
+     }
+  </style>
+</head>
+<body>
+  <div class='h1'>
+ <h1><span style="color: black">WELCOME </span><br><br> HNG INTERNSHIP 4</h1>
+ </div>
+
+<div id="date">
+	<?php
+echo date('h:i:s A');
+  ?>	
+</div>
+  
+</body>
+</html>
